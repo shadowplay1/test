336a328ae61b6a86938ad56c69e22c2dea06fce1
shadowplay1 <43500997+shadowplay1@users.noreply.github.com>
2021-08-13 20:04:38 +0300

    Add files via upload

---
+++ 1.3.8/src/structures/errors.js
@@ -0,0 +1,53 @@
+module.exports = {
+    notReady: 'The module is not ready to work.',
+
+    invalidTypes: {
+        memberID: 'memberID must be a string. Received type: ',
+        guildID: 'guildID must be a string. Received type: ',
+        amount: 'amount must be a number. Received type: ',
+
+        addItemOptions: {
+            itemName: 'options.itemName must be a string. Received type: ',
+            price: 'options.price must be a number. Received type: ',
+            message: 'options.message must be a string. Received type: ',
+            description: 'options.description must be a string. Received type: ',
+            maxAmount: 'options.maxAmount must be a number. Received type: ',
+            role: 'options.role must be a string. Received type: '
+        },
+
+        editItemArgs: {
+            itemID: 'itemID is not a string or a number. Received type: ',
+            arg: `'arg' parameter must be one of these values: ${['description', 'price', 'itemName', 'message', 'maxAmount', 'role'].map(x => `'${x}'`).join(', ')}. Received: `,
+            noValue: 'no value specified. Received: '
+        },
+    },
+
+    workAmount: {
+        tooManyElements: 'options.workAmount array cannot have more than 2 elements; it must have min and max values as first and second element of the array (example: [10, 20]).',
+    },
+
+    databaseManager: {
+        invalidTypes: {
+            key: 'Key must be a string. Received type: ',
+            target: {
+                number: 'Target is not a number. Received type: ',
+                array: 'Target is not an array. Received type: '
+            },
+            value: {
+                number: 'Value is not a number. Received type: ',
+                array: 'Value is not an array. Received type: '
+            }
+        }
+    },
+
+    noClient: 'You need to specify your bot client to give roles to members.',
+    roleNotFound: 'Could not find a role with ID ',
+
+    oldNodeVersion: 'This module is supporting only Node.js v14 or newer. Installed version is ',
+
+    reservedName(name = 'testStorage123') {
+        return `'${name}' is a reserved storage file name. You cannot use it.`
+    },
+    invalidStorage: 'Storage file is not valid.',
+    wrongStorageData: 'Storage file contains wrong data.',
+}
